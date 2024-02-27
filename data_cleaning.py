3e1207dedd9494afcf1d54196998f40860ce5cd3
oscarwilkins1707 <oscarjwilkins@gmail.com>
2024-02-27 15:55:54 +0100

    pushing the correlation matrix

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 df =  pd.read_csv('HeartDisease.csv')
 print('check')
 print(df)
+df = df.drop('chd',axis=1)
+#print(df.describe().round(2))
 #HELLO WORLD
+
+
+corr_matrix = df.corr()
+plt.figure(figsize=(8, 6))
+sns.heatmap(corr_matrix, annot=True, cmap='Blues', fmt=".2f")
+plt.title('Correlation Matrix')
+plt.show()
